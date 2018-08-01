64c2b94daa90afcd7c6ffb19c0b8da9201541b41
Nowak Dominik <architektpiramid@gmail.com>
2018-08-01 21:29:22 +0200

    New project - Weather Station

--- SimpleBoardTest/text/text.ino
+++ SimpleBoardTest/text/text.ino
@@ -1,37 +1,48 @@
 
 // VERSION 0.1
 
+#include "SimpleDHT.h"   // DHT sensor by Winlin
+
 // ----- CONFIG
-    // VALUES
-    static boolean status = true;
-
-    // PC LOGS
-    const boolean serialStatus = true;
-    const uint16_t baudRate = 9600;
+    #define BAUD_RATE 9600
+    #define DHTTYPE DHT11
 
 
-void ledBlink(uint16_t ON, uint16_t OFF, bool status = true) {
-    while (status) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(ON);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(OFF);
-    }
+void ledBlink(uint16_t ON, uint16_t OFF, uint16_t times = 2) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(ON);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(OFF);
 }
+
 // ------ END CONFIG
 
+SimpleDHT11 dht11;
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(baudRate);
-    ledBlink(150, 150);       // wait until serial has been open
+    pinMode(LED_BUILTIN, OUTPUT);       // Init board blink led
+    Serial.begin(BAUD_RATE);            // Init serial monitor
+  
     while(!Serial) {
-        Serial.println("[+] Serial is now active!");
-        ledBlink(0, 0, false);
+        // Wait until terminal has been open
     }
-    
+    Serial.println("[+] Serial Monitor succesfully initialized!");
 }
 
 
 void loop() {
-
+     byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(A6, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
 }
---
+++ WeatherStation/main.ino
@@ -0,0 +1,84 @@
+// VERSION 0.1
+
+#include <SimpleDHT.h>   // DHT sensor by Winlin
+#include <stdio.h>
+#include <DS1302.h>
+
+// ----- CONFIG
+    #define BAUD_RATE 9600
+
+    // PINS
+    #define pinD_DHT 7
+    const int kCePin   = 4;  // Chip Enable
+    const int kIoPin   = 2;  // Input/Output
+    const int kSclkPin = 3;  // Serial Clock
+
+    // CLASSES
+    SimpleDHT11 DHT;
+    DS1302 rtc(kCePin, kIoPin, kSclkPin);
+
+
+void ledBlink(uint16_t ON, uint16_t OFF, uint16_t times = 2) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(ON);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(OFF);
+}
+
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "Sunday";
+    case Time::kMonday: return "Monday";
+    case Time::kTuesday: return "Tuesday";
+    case Time::kWednesday: return "Wednesday";
+    case Time::kThursday: return "Thursday";
+    case Time::kFriday: return "Friday";
+    case Time::kSaturday: return "Saturday";
+  }
+  return "(unknown day)";
+  }
+
+void printTime() {
+  Time t = rtc.time();
+
+  const String day = dayAsString(t.day);
+  char buf[50];
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+
+  Serial.println(buf);
+}
+// ------ END CONFIG
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);       // Init board blink led
+    Serial.begin(BAUD_RATE);            // Init serial monitor
+  
+    while(!Serial) {
+        // Wait until terminal has been open
+    }
+    Serial.println("[+] Serial Monitor succesfully initialized!");
+}
+
+
+void loop() {
+
+unsigned char temperature = 0;
+unsigned char humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = DHT.read(pinD_DHT, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  printTime();
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  
+  delay(2000);
+
+    // id, date, temp, hum, foto
+}
