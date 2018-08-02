1be8a8b47382771af47db8ad9957c27439e6ac74
Nowak Dominik <architektpiramid@gmail.com>
2018-08-02 19:58:09 +0200

    v0.2 - new sensors

--- WeatherStation/main.ino
+++ WeatherStation/main.ino
@@ -3,12 +3,15 @@
 #include <SimpleDHT.h>   // DHT sensor by Winlin
 #include <stdio.h>
 #include <DS1302.h>
+#include <SPI.h>
+#include <SD.h>
 
 // ----- CONFIG
     #define BAUD_RATE 9600
-
+    #define SAVE_EVERY_MIN 10
     // PINS
     #define pinD_DHT 7
+    #define pin_PRES A6
     const int kCePin   = 4;  // Chip Enable
     const int kIoPin   = 2;  // Input/Output
     const int kSclkPin = 3;  // Serial Clock
@@ -16,7 +19,7 @@
     // CLASSES
     SimpleDHT11 DHT;
     DS1302 rtc(kCePin, kIoPin, kSclkPin);
-
+    File myFile;
 
 void ledBlink(uint16_t ON, uint16_t OFF, uint16_t times = 2) {
     digitalWrite(LED_BUILTIN, HIGH);
@@ -38,7 +41,7 @@ String dayAsString(const Time::Day day) {
   return "(unknown day)";
   }
 
-void printTime() {
+String getFullTime() {
   Time t = rtc.time();
 
   const String day = dayAsString(t.day);
@@ -48,11 +51,23 @@ void printTime() {
            t.yr, t.mon, t.date,
            t.hr, t.min, t.sec);
 
-  Serial.println(buf);
+  return buf;
+}
+
+String getDateString() {
+  Time t = rtc.time();
+
+  const String day = dayAsString(t.day);
+  char buf[12];
+  snprintf(buf, sizeof(buf),  "%04d-%02d-%02d",
+           t.yr, t.mon, t.date);
+
+  return buf;
 }
 // ------ END CONFIG
 
 void setup() {
+    pinMode(pin_PRES, INPUT);           // for photoresistor
     pinMode(LED_BUILTIN, OUTPUT);       // Init board blink led
     Serial.begin(BAUD_RATE);            // Init serial monitor
   
@@ -60,30 +75,51 @@ void setup() {
         // Wait until terminal has been open
     }
     Serial.println("[+] Serial Monitor succesfully initialized!");
+
+    if (!SD.begin(A0)) {
+      Serial.println("initialization failed!");
+      return;
+  }
+  Serial.println("[+] SD Card initialization done.");
+}
+
+void saveToSD(String time, int8_t temp, uint8_t hum) {
+  String a = time;
+  a += ",";
+  a += temp;
+  a += ",";
+  a += hum;
+  myFile = SD.open("teeest.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println(a);
+    myFile.close();
+    Serial.print("[+] Added to file: ");
+    Serial.println(a);
+  }
 }
 
 
 void loop() {
 
-unsigned char temperature = 0;
-unsigned char humidity = 0;
+  unsigned char temperature = 0;
+  unsigned char humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = DHT.read(pinD_DHT, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
     return;
   }
+ // Serial.println(getDateString());
   
-   char buf[100];
-   Time t = rtc.time();
-  snprintf(buf, sizeof(buf),  "%04d-%02d-%02d %02d:%02d,%02d,%02d",
-  
-           t.yr, t.mon, t.date,
-           t.hr, t.min, temperature, humidity);
+  //Serial.println(getFullTime());
+  saveToSD(getFullTime(), temperature, humidity);
 
-  Serial.println(buf);        
 
-  
-  delay(2 * 1000);
+  delay(SAVE_EVERY_MIN * (60000));
 
     // id, date, temp, hum, foto
+}
+
+float getSunPrec() {
+  int value = analogRead(pin_PRES);
+  return (map(value, 0, 1023, 1000, 0) / 10.0);
 }
